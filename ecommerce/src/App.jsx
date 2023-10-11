b9a1e1349cf5a438011906a4bdc139f641ea165d
unknown <msalani98@gmail.com>
2023-10-11 11:06:46 -0300

    funcion carrito

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar/NavBar'
 import './App.css'
 import "./components/ItemListContainer/ItemListContainer"
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
+import ItemCount from "./components/ItemCount/ItemCount"
 function App() {
   const [count, setCount] = useState(0)
 
@@ -11,6 +12,7 @@ function App() {
       <div className='App'>
         <NavBar/>
         <ItemListContainer greeting={"bienvenidxs"}/>
+        <ItemCount initial={1} stock={10} onAdd={(quantity) => console.log("cantidad agregada ",quantity)}/>
       </div>
 
     </>
