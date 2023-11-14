ec5275d03dfc119dc2d10d4aa7e1da02a4f13cf8
unknown <msalani98@gmail.com>
2023-11-14 12:35:49 -0300

    conexion firebase

---
+++ ecommerce/src/Services/Firebase/firebaseConfig.js
@@ -0,0 +1,18 @@
+
+import { initializeApp } from "firebase/app";
+import { getFitestore } from "firebase/firestore"
+import { getAuth} from "firebase/auth"
+const firebaseConfig = {
+  apiKey: "AIzaSyD3ETeRF-WNmhtitrAscRaI9u5csB-2Puk",
+  authDomain: "ecommerce-3672e.firebaseapp.com",
+  projectId: "ecommerce-3672e",
+  storageBucket: "ecommerce-3672e.appspot.com",
+  messagingSenderId: "728224789784",
+  appId: "1:728224789784:web:877eb77f4c42496cce5a6d"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+ export const db = getFitestore(app)
+
