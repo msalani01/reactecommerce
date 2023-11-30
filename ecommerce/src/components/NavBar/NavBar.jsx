3426ab5659880c1c3b2f543aeecb01eb8c7ef609
unknown <msalani98@gmail.com>
2023-11-30 08:59:39 -0300

    categorias implementadas

--- ecommerce/src/components/NavBar/NavBar.jsx
+++ ecommerce/src/components/NavBar/NavBar.jsx
@@ -1,5 +1,6 @@
 import CartWidget from "../CartWidget/CartWidget";
-import { NavLink, Link } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom';
+import './NavBar.css'; // Import del archivo CSS
 
 const NavBar = () => {
     return (
@@ -8,13 +9,12 @@ const NavBar = () => {
                 <h3>Libroverso</h3>
             </Link>
             <div className="Categories">
-                
                 <NavLink to={'/category/Libros'} activeClassName='ActiveOption'>Libros</NavLink>
                 <NavLink to={'/category/Juegosdemesa'} activeClassName='ActiveOption'>Juegos de mesa</NavLink>
                 <NavLink to={'/category/Comics'} activeClassName='ActiveOption'>Comics</NavLink>
             </div>
         </nav>
-    )
+    );
 }
 
-export default NavBar
+export default NavBar;
