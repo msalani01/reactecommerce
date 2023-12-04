7a4b6d4d8784e5b07aa3a182cfe2702ea17a8de5
unknown <msalani98@gmail.com>
2023-12-04 13:08:51 -0300

    total

--- ecommerce/src/components/Cart/Cart.jsx
+++ ecommerce/src/components/Cart/Cart.jsx
@@ -1,28 +1,35 @@
-import './Cart.css'
-import { useContext } from 'react'
-import { CartContext } from '../../CartContext/CartContext'
-import CartItem from '../CartItem/CartItem'
-import { Link } from 'react-router-dom'
+import React, { useContext } from 'react';
+import { CartContext } from '../../CartContext/CartContext';
+import CartItem from '../CartItem/CartItem';
+import { Link } from 'react-router-dom';
 
 const Cart = () => {
-    const { cart, clearCart, totalQuantity, total } = useContext(CartContext)
+  const { cart, clearCart, totalQuantity, total } = useContext(CartContext);
 
-    if (totalQuantity === 0) {
-        return (
-            <div>
-                <Link to='/' className='Option'>Productos</Link>
-            </div>
-        )
-    }
-
+  if (totalQuantity === 0) {
     return (
-        <div>
-            {cart.map(p => <CartItem key={p.id} {...p} />)}
-            <h3>Total: ${total}</h3>
-            <button onClick={() => clearCart()} className='Button'>Limpiar carrito</button>
-            <Link to='/checkout' className='Option'>Checkout</Link>
-        </div>
-    )
-}
+      <div>
+        <Link to='/' className='Option'>Productos</Link>
+      </div>
+    );
+  }
 
-export default Cart
+  return (
+    <div>
+      {cart.map(product => (
+        <CartItem
+          key={product.id}
+          id={product.id}
+          name={product.name}
+          price={product.price}
+          quantity={product.quantity}
+        />
+      ))}
+      <h3>Total: ${total}</h3>
+      <button onClick={() => clearCart()} className='Button'>Limpiar carrito</button>
+      <Link to='/checkout' className='Option'>Checkout</Link>
+    </div>
+  );
+};
+
+export default Cart;
