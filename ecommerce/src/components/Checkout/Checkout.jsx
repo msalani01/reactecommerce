ec2c25e86a66f509e9eea6d6bde9281f766fde73
unknown <msalani98@gmail.com>
2023-12-04 10:05:01 -0300

    ya da el ID

--- ecommerce/src/components/Checkout/Checkout.jsx
+++ ecommerce/src/components/Checkout/Checkout.jsx
@@ -1,18 +1,35 @@
 import React, { useState } from 'react';
 import CheckoutForm from '../CheckoutForm/CheckoutForm';
+import { addDoc, collection } from 'firebase/firestore';
+import { db } from '../../Services/Firebase/firebaseConfig';
 
 const Checkout = () => {
-  const handleOrderSubmit = (formData) => {
- 
-    console.log('Datos de la orden: ', formData);
+  const [orderId, setOrderId] = useState(null);
 
+  const handleOrderSubmit = async (formData) => {
+    try {
+     
+      const docRef = await addDoc(collection(db, 'orders'), formData);
 
+      
+      const generatedOrderId = docRef.id;
+
+      
+      setOrderId(generatedOrderId);
+
+      console.log('Orden creada con ID:', generatedOrderId);
+    } catch (error) {
+      console.error('Error al crear la orden:', error);
+    }
   };
 
   return (
     <div>
-    
-      <CheckoutForm onSubmit={handleOrderSubmit} />
+      {orderId ? (
+        <p>¡Pedido realizado con éxito! ID de orden: {orderId}</p>
+      ) : (
+        <CheckoutForm onSubmit={handleOrderSubmit} />
+      )}
     </div>
   );
 };
